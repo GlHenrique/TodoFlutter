439274dd5e68d4d665fd339ee33ab54f53fce882
Glênio Carvalho <gleniocarvalho@MacBook-Pro-de-Glenio.local>
2022-02-09 21:37:50 -0300

    feat: add clear all button

--- lib/pages/todo_list.dart
+++ lib/pages/todo_list.dart
@@ -9,26 +9,51 @@ class TodoListPage extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Row(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              const Expanded(
-                  child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Adicione uma tarefa',
-                ),
-              )),
-              const SizedBox(width: 8),
-              ElevatedButton(
-                  onPressed: () {},
-                  child: const Icon(
-                    Icons.add,
-                    size: 30,
+              Row(
+                children: [
+                  const Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Adicione uma tarefa',
+                      ),
+                    ),
                   ),
-                  style: ElevatedButton.styleFrom(
-                    primary: const Color(0xff00d7f3),
-                    padding: const EdgeInsets.all(15),
-                  )),
+                  const SizedBox(width: 8),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Icon(
+                      Icons.add,
+                      size: 30,
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color(0xff00d7f3),
+                      padding: const EdgeInsets.all(15),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Row(
+                children: [
+                  const Expanded(
+                    child: Text('Você possui 0 tarefas pendentes'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Limpar tudo'),
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color(0xff00d7f3),
+                      padding: const EdgeInsets.all(15),
+                    ),
+                  )
+                ],
+              )
             ],
           ),
         ),
